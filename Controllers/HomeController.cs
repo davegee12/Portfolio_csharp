79fc36bb5c45e51755c6528a38096298676d4afe
davegee12 <davidgee1234@gmail.com>
2019-07-04 12:17:05 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers     //be sure to use your own project's namespace!
+{
+    public class HelloController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet("")]       //type of request
+        public string Index()
+        {
+            return "This is my Index!";
+        }
+        [HttpGet("projects")]
+        public string Projects()
+        {
+            return "These are my projects!";
+        }
+        [HttpGet("contact")]
+        public string Contact()
+        {
+            return "This is my contact!";
+        }
+    }
+}
