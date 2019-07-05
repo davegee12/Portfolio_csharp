3864981bde069ac9423795c3486e03858dc09df2
davegee12 <davidgee1234@gmail.com>
2019-07-05 11:26:29 -0500

    second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,23 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolio.Controllers     //be sure to use your own project's namespace!
 {
-    public class HelloController : Controller   //remember inheritance??
+    public class HomeController : Controller   //remember inheritance??
     {
         //for each route this controller is to handle:
         [HttpGet("")]       //type of request
-        public string Index()
+        public ViewResult Index()
         {
-            return "This is my Index!";
+            return View();
         }
         [HttpGet("projects")]
-        public string Projects()
+        public ViewResult Projects()
         {
-            return "These are my projects!";
+            return View();
         }
         [HttpGet("contact")]
-        public string Contact()
+        public ViewResult Contact()
         {
-            return "This is my contact!";
+            return View();
         }
     }
 }
